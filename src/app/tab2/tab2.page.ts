8d3bf3fd10022880fc27c9f49f41d2b969238d0b
Shaun-Mifsud <32672907+Shaun-Mifsud@users.noreply.github.com>
2021-05-26 11:43:12 +0200

    Calendar
    categories update

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,9 @@
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 import { Component } from '@angular/core';
+import { ListCreationService } from '../services/list-creation.service';
+import { StorageService } from '../services/storage.service';
+import { TaskService } from '../services/task.service';
+import {  Category, Task } from '../struct/list';
 
 @Component({
   selector: 'app-tab2',
@@ -21,9 +26,50 @@ export class Tab2Page {
       slideShadows: true,
     }
   }
-  
-  constructor() {
 
+  public item: Task = { name: '', complete: false };
+  public categories: Category[] = [];
+  public currentTasks: Task[] = [];
+
+  //cateogry ID
+  selectedIndex: number = -1;
+
+  slideNumber: number = 0;
+
+  categoryIndex:number = -1;
+
+  
+  constructor(
+    private storageService: StorageService,
+    public taskService: TaskService
+  ) {}
+
+  async ngOnInit()
+  {
+    this.categories = this.taskService.list;
+    //console.log(this.categories);
   }
+
+  chooseCategory(index:number){
+    this.categoryIndex = index;
+    //console.log(this.categories[this.categoryIndex]);
+  }
+
+  getTasks(index:number){
+    this.currentTasks = this.taskService.list[index].tasks;  
+    console.log(this.currentTasks);  
+  } 
+
+  updateCheckbox(task:Task)
+  {
+    this.taskService.writeToStorage();
+    
+    
+    //this.list[categoryIndex].tasks.push(updateItem);
+    //rewrite the list
+    //this.writeToStorage();
+  }
+
+  
 
 }
